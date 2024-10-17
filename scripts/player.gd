b4b64881ae7652a79bdec4e1fad632f8cf05426f
Tamires <tamires.ei04@gmail.com>
2024-10-17 20:47:35 -0300

    Update player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,21 +2,10 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-
-#signal player_has_died()
-
-#func _process(delta):
-	#queue_free()
-	#emit_signal("player_has_died")
 	
 func _physics_process(_delta):
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionx := Input.get_axis("ui_left", "ui_right")
 	var direciony := Input.get_axis("ui_up", "ui_down")
 	if directionx:
@@ -25,7 +14,5 @@ func _physics_process(_delta):
 	if direciony:
 		velocity.y = direciony * SPEED
 		velocity.x = directionx * 0
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
